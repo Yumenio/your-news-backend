bfbb6510d16f641085485d0e5776521463f9be6f
Yumenio <alejandroandy447@gmail.com>
2024-01-19 10:58:10 -0500

    feat: registered ApConfiguration values at startup, and added depedency injection

--- News.Api/Program.cs
+++ News.Api/Program.cs
@@ -1,7 +1,13 @@
+using News.Api.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+AppConfiguration config = new AppConfiguration(
+    builder.Configuration["ApiKey"],
+    builder.Configuration["TopHeadlinesUrl"]);
 
+builder.Services.AddSingleton<AppConfiguration>(config);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
