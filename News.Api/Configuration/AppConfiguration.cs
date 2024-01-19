0001d3fe1d9ee0669632887cefffb0b8ce9e8b95
Yumenio <alejandroandy447@gmail.com>
2024-01-19 10:57:13 -0500

    feat: added AppConfiguration class for storing secrets

---
+++ News.Api/Configuration/AppConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace News.Api.Configuration
+{
+    public class AppConfiguration
+    {
+        public string ApiKey { get; set; }
+        public string TopHeadlinesUrl { get; set; }
+        public AppConfiguration(string apiKey, string topHeadlinesUrl)
+        {
+            ApiKey = apiKey;
+            TopHeadlinesUrl = topHeadlinesUrl;
+        }
+    }
+}
