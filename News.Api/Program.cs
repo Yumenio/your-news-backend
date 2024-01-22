4b1bdbba9d05f699028592ef7ff431c2410efa61
Yumenio <alejandroandy447@gmail.com>
2024-01-22 18:09:54 -0500

    feat: added CORS for accepting localhost ReactApp requests

--- News.Api/Program.cs
+++ News.Api/Program.cs
@@ -12,6 +12,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", builder => builder.WithOrigins("http://localhost:3000")
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -20,6 +26,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("AllowReactApp");
 }
 
 app.UseHttpsRedirection();
