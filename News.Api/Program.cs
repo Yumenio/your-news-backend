b5d5e0a930c129cb45007cac62aeb36a7073be4d
Yumenio <alejandroandy447@gmail.com>
2024-01-23 19:29:15 -0500

    feat: adds AuthSecret to the AppConfiguration in the pipeline

--- News.Api/Program.cs
+++ News.Api/Program.cs
@@ -1,11 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using News.Api.Configuration;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 AppConfiguration config = new AppConfiguration(
     builder.Configuration["ApiKey"],
-    builder.Configuration["TopHeadlinesUrl"]);
+    builder.Configuration["TopHeadlinesUrl"],
+    builder.Configuration["AuthSecret"]);
 
 builder.Services.AddSingleton<AppConfiguration>(config);
 builder.Services.AddControllers();
@@ -17,6 +21,25 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp", builder => builder.WithOrigins("http://localhost:3000")
     .AllowAnyMethod()
     .AllowAnyHeader());
+});
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            config.AuthSecret)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
 });
 
 var app = builder.Build();
@@ -31,6 +54,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
